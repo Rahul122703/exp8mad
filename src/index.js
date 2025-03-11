2fb2bafed90af2f6f51606ae4f588e1ddaa20360
rahul122703 <killbusyness@gmail.com>
2025-03-11 10:19:24 +0530

    added installation option

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js') 
+    .then((reg) => {
+      console.log("Service Worker registered", reg);
+    })
+    .catch((err) => {
+      console.log("Service Worker not registered", err);
+    });
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
